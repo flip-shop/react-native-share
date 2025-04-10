c52fec0a68ef8131f8f6b3896d12c1ccfd8b18d2
Krzysztof Gajewski <krzysztof.gajewski@flip.shop>
2025-04-10 10:29:48 +0200

    android patch part

---
+++ android/src/main/java/cl/json/DefaultActivityLifecycleCallbacks.java
@@ -0,0 +1,41 @@
+package cl.json;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public abstract class DefaultActivityLifecycleCallbacks implements Application.ActivityLifecycleCallbacks {
+
+    @Override
+    public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(@NonNull Activity activity) {
+
+    }
+}
